662bfcf5ee54639bdc5c7678b9626c5011f62dd3
032 - Abhishek Choksi <19bmiit032@gmail.com>
2022-03-16 14:14:02 +0530

    Notification Manager Application @ 2022 #032AbhishekChoksi. All rights reserved.

---
+++ app/src/main/java/com/abhishekchoksi/notificationmanagerapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.abhishekchoksi.notificationmanagerapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String CHANNEL_ID = "group_1";
+    String CHANNEL_NAME = "group";
+    Integer NOTIFICATION_ID = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID);
+
+        builder.setContentTitle("Hello");
+        builder.setContentText("Hello This is notification");
+        builder.setSmallIcon(R.drawable.ic_launcher_background);
+        builder.setAutoCancel(true);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+        managerCompat.notify(NOTIFICATION_ID, builder.build());
+    }
+}
